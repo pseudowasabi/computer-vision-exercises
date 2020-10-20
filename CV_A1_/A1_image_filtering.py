0353ec4fd32a54a4b47ebf0cecec3b52d30f73a4
chocolateWasabi <y0s30b.k@gmail.com>
2020-10-20 19:35:52 +0900

    modify gaussian kernels

--- CV_A1_/A1_image_filtering.py
+++ CV_A1_/A1_image_filtering.py
@@ -5,7 +5,8 @@ A1_image_filtering.py
 
 import cv2
 import numpy as np
-# not allowed to use any assignment-related numpy built-in functions
+import math
+from scipy.ndimage import gaussian_filter
 
 '''
 Step 1. 
@@ -21,40 +22,46 @@ def cross_correlation_1d(img, kernel):
         if kernel.shape[1] == 1: # vertical kernel
             dir = 1
         kernel = kernel.flatten() # flatten to 1-d ndarray
-    img_size = img.shape[0]
+    #img_size_0 = img.shape[0]
+    #img_size_1 = img.shape[1]
     kernel_size = kernel.shape[0]
 
     # 2. padding original image (img)
-    padded_size = img_size + kernel_size - 1
+    #padded_size = img_size_0 + kernel_size - 1
     if dir == 0:
-        padded_img = np.zeros((img_size, padded_size))
-    elif dir == 1:
-        padded_img = np.zeros((padded_size, img_size))
+        padded_size = img.shape[1] + kernel_size - 1
+        padded_img = np.zeros((img.shape[0], padded_size))
 
-    for i in range(img_size):
-        for j in range(padded_size):
-            j_prime = j - (kernel_size // 2)
-            if 0 <= j_prime < img_size:
-                if dir == 0:
+        for i in range(img.shape[0]):
+            for j in range(padded_size):
+                j_prime = j - (kernel_size // 2)
+                if 0 <= j_prime < img.shape[1]:
                     padded_img[i][j] = img[i][j_prime]
-                elif dir == 1:
-                    padded_img[j][i] = img[j_prime][i]
-            elif j_prime < 0:
-                if dir == 0:
-                   padded_img[i][j] = img[i][0]
-                elif dir == 1:
-                    padded_img[j][i] = img[0][i]
-            elif j_prime >= img_size:
-                if dir == 0:
-                    padded_img[i][j] = img[i][img_size - 1]
-                elif dir == 1:
-                    padded_img[j][i] = img[img_size - 1][i]
+                elif j_prime < 0:
+                    padded_img[i][j] = img[i][0]
+                else:
+                    padded_img[i][j] = img[i][img.shape[1]-1]
+    elif dir == 1:
+        padded_size = img.shape[0] + kernel_size - 1
+        padded_img = np.zeros((padded_size, img.shape[1]))
+
+        for j in range(img.shape[1]):
+            for i in range(padded_size):
+                i_prime = i - (kernel_size // 2)
+                if 0 <= i_prime < img.shape[0]:
+                    padded_img[i][j] = img[i_prime][j]
+                elif i_prime < 0:
+                    padded_img[i][j] = img[0][j]
+                else:
+                    padded_img[i][j] = img[img.shape[0]-1][j]
+
     print(padded_img)
     print()
+
     # 3. apply cross correlation using iteration
-    filtered_img = np.zeros((img_size, img_size))
-    for i in range(img_size):
-        for j in range(img_size):
+    filtered_img = np.zeros((img.shape[0], img.shape[1]))
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
             if dir == 0:
                 filtered_img[i][j] = sum(np.multiply(padded_img[i][j:j+kernel_size], kernel))
             elif dir == 1:
@@ -69,48 +76,48 @@ def cross_correlation_1d(img, kernel):
     #print(filtered_img)
 
     filtered_img = filtered_img.astype('uint8')
-    return filtered_img
+    return filtered_img # need to be checked
 
 
 def cross_correlation_2d(img, kernel):
     # 1. padding image
-    img_size = img.shape[0]
+    img_size = [img.shape[0], img.shape[1]]
     kernel_size = kernel.shape[0]
-    padded_size = img_size + kernel_size - 1
-    padded_img = np.zeros((padded_size, padded_size))
+    padded_size = [img_size[0] + kernel_size - 1, img_size[1] + kernel_size - 1]
+    padded_img = np.zeros((padded_size[0], padded_size[1]))
 
-    for i in range(padded_size):
+    for i in range(padded_size[0]):
         i_prime = i - (kernel_size // 2)
-        for j in range(padded_size):
+        for j in range(padded_size[1]):
             j_prime = j - (kernel_size // 2)
-            if 0 <= i_prime < img_size:
-                if 0 <= j_prime < img_size:
+            if 0 <= i_prime < img_size[0]:
+                if 0 <= j_prime < img_size[1]:
                     #padded_img[i][j] = -img[i_prime][j_prime]
                     padded_img[i][j] = img[i_prime][j_prime]
                 elif j_prime < 0: # West
                     padded_img[i][j] = img[i_prime][0]
-                elif j_prime >= img_size: # East
-                    padded_img[i][j] = img[i_prime][img_size - 1]
+                elif j_prime >= img_size[1]: # East
+                    padded_img[i][j] = img[i_prime][img_size[1] - 1]
             elif i_prime < 0:
-                if 0 <= j_prime < img_size: # North
+                if 0 <= j_prime < img_size[1]: # North
                     padded_img[i][j] = img[0][j_prime]
                 elif j_prime < 0: # NW
                     padded_img[i][j] = img[0][0]
-                elif j_prime >= img_size: # NE
-                    padded_img[i][j] = img[0][img_size - 1]
-            elif i_prime >= img_size:
-                if 0 <= j_prime < img_size: # South
-                    padded_img[i][j] = img[img_size - 1][j_prime]
+                elif j_prime >= img_size[1]: # NE
+                    padded_img[i][j] = img[0][img_size[1] - 1]
+            elif i_prime >= img_size[0]:
+                if 0 <= j_prime < img_size[1]: # South
+                    padded_img[i][j] = img[img_size[0] - 1][j_prime]
                 elif j_prime < 0: # SW
-                    padded_img[i][j] = img[img_size - 1][0]
-                elif j_prime >= img_size: # SE
-                    padded_img[i][j] = img[img_size - 1][img_size - 1]
+                    padded_img[i][j] = img[img_size[0] - 1][0]
+                elif j_prime >= img_size[1]: # SE
+                    padded_img[i][j] = img[img_size[0] - 1][img_size[1] - 1]
     #print(padded_img)
 
     # 2. apply cross correlation using iteration
-    filtered_img = np.zeros((img_size, img_size))
-    for x in range(img_size):
-        for y in range(img_size):
+    filtered_img = np.zeros((img_size[0], img_size[1]))
+    for x in range(img_size[0]):
+        for y in range(img_size[1]):
             # at each (x, y) point, cross correlation using kernel is calculated.
             for i in range(x, x + kernel_size):
                 for j in range(y, y + kernel_size):
@@ -120,29 +127,48 @@ def cross_correlation_2d(img, kernel):
     #print(filtered_img)
 
     filtered_img = filtered_img.astype('uint8')
-    return filtered_img
+    return filtered_img # need to be checked
 
-# if np.arange, sqrt, pi, exp, multiply functions are not allowed to use,
-# below code should be modified !!!
 def get_gaussian_filter_1d(size, sigma):
-    # return gaussian filter for horizontal 1D
-    kernel = np.arange(-(size//2), size//2+1)
-    kernel = (1 / (np.sqrt(2 * np.pi) * sigma)) * np.exp((-1) * np.multiply(kernel, kernel) / (2 * (sigma ** 2)))
-    '''
+    # return gaussian filter for horizontal 1-d
+    x = np.linspace(-size//2+1, size//2, size)
+    kernel = np.zeros((size))
+
+    coeff1 = 1 / (math.sqrt(2 * math.pi) * sigma)
+    coeff2 = -(1 / (2 * (sigma ** 2)))
+    _sum = 0
     for i in range(size):
-        print('%.04f'%(kernel[i]))
-    '''
+        kernel[i] = coeff1 * math.exp(coeff2 * (x[i] ** 2))
+        #print('%.04f'%kernel[i], end=' ')
+        _sum += kernel[i]
+
+    coeff_normalize = 1 / _sum
+    for i in range(size): # normalize
+        kernel[i] *= coeff_normalize
     return kernel
 
 def get_gaussian_filter_2d(size, sigma):
-    kernel_1d = get_gaussian_filter_1d(size, sigma)
+    # return gaussian filter for 2-d
+    x = np.linspace(-size//2+1, size//2, size)
     kernel = np.zeros((size, size))
-    #print(kernel.shape)
+
+    coeff1 = 1 / (2 * math.pi * (sigma ** 2))
+    coeff2 = -(1 / (2 * (sigma ** 2)))
+    for i in range(size):
+        x[i] = coeff2 * (x[i] ** 2)
+
+    _sum = 0
     for i in range(size):
         for j in range(size):
-            kernel[i][j] = kernel_1d[i] * kernel_1d[j]
-            #print('%.04f'%kernel[i][j], end = ' ')
+            kernel[i][j] = coeff1 * math.exp(x[i] + x[j])
+            #print('%.04f'%kernel[i][j], end=' ')
+            _sum += kernel[i][j]
         #print()
+
+    coeff_normalize = 1 / _sum
+    for i in range(size):
+        for j in range(size):
+            kernel[i][j] *= coeff_normalize
     return kernel
 
 '''
@@ -152,43 +178,43 @@ load images in grayscale
 
 img_lenna = cv2.imread('./lenna.png', cv2.IMREAD_GRAYSCALE)
 img_shapes = cv2.imread('./shapes.png', cv2.IMREAD_GRAYSCALE)
-# cv2.imshow("Lenna in grayscale", img_lenna)
-# cv2.imshow("Shapes", img_shapes)
-print(img_lenna.shape)
 
+k_size = [5, 11, 17]
+sigmas = [1, 6, 11]
+font = cv2.FONT_HERSHEY_SIMPLEX
+final_filtered_lenna = np.zeros((img_lenna.shape[0]*3, img_lenna.shape[1]*3))
+final_filtered_shapes = np.zeros((img_shapes.shape[0]*3, img_shapes.shape[1]*3))
 
-'''
-Step 3.
-implementation of requirements
-'''
+for i in range(1):
+    for j in range(1):
+        #k = k_size[i]
+        #s = sigmas[j]
+        k = 17
+        s = 11
+        kernel = get_gaussian_filter_2d(k, s)
 
-#get_gaussian_filter_2d(9, 1)
+        filtered_img_lenna = cross_correlation_2d(img_lenna, kernel)
+        #filtered_img_shapes = cross_correlation_2d(img_shapes, kernel)
 
-size = 8
-img_a = np.arange(-(size//2), (size//2) + 1)
-img = np.zeros((size, size))
-for i in range(size):
-    for j in range(size):
-        img[i][j] = img_a[i] * img_a[j]
-#print(img.shape)
-#print("original img")
-#print(img)
-cv2.imshow("Lenna original", img_lenna)
+        cv2.putText(filtered_img_lenna, '{0}x{0}, s={1}'.format(k, s), (30, 30), font, 1, (0, 0, 0), 2, cv2.LINE_AA)
+        #cv2.putText(filtered_img_shapes, '{0}x{0}, s={1}'.format(k, s), (30, 30), font, 1, (0, 0, 0), 2, cv2.LINE_AA)
 
-'''
-# 1-d kernel cross correlationing
-kernel = get_gaussian_filter_1d(5, 1)
-kernel = np.array([kernel]).transpose()
-print(kernel)
-cross_correlation_1d(img, kernel)
-'''
+        #for x in range(img_lenna.shape[0]*i, img_lenna.shape[0]*(i+1)):
+        #    for y in range(img_lenna.shape[1]*i, img_lenna.shape[1]*(i+1)):
+        #        final_filtered_lenna[x][y] = filtered_img_lenna[x-img_lenna.shape[0]*i][y-img_lenna.shape[1]*i]
+        print('{0}x{0}, s={1}'.format(k, s))
+        cv2.imshow("aa", filtered_img_lenna)
+        #cv2.waitKey(0)
+        #for x in range(img_shapes.shape[0]*i, img_shapes.shape[0]*(i+1)):
+        #    for y in range(img_shapes.shape[1]*i, img_shapes.shape[1]*(i+1)):
+        #        final_filtered_shapes[x][y] = filtered_img_shapes[x-img_shapes.shape[0]*i][y-img_shapes.shape[1]*i]
 
-kernel = get_gaussian_filter_2d(5, 1)
-filtered_img_lenna = cross_correlation_2d(img_lenna, kernel)
-#print("filtered_img")
-#print(filtered_img)
-cv2.imshow("Lenna Gaussian filtered", filtered_img_lenna)
-print(filtered_img_lenna.shape)
+#cv2.imshow("Gaussian filtering of Lenna by Yoseob Kim", final_filtered_lenna)
+#cv2.imshow("Gaussian filtering of Shapes by Yoseob Kim", final_filtered_shapes)
+
+blur = cv2.GaussianBlur(img_lenna, (5, 5), 11)
+#cv2.imshow("by cv2", blur)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
