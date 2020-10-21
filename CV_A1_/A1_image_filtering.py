88415acb69bfe08757c3073e34635f2320c6a591
chocolateWasabi <y0s30b.k@gmail.com>
2020-10-21 10:55:28 +0900

    change kernel(2d>1d) in 3x3 filtering, normalize intensity value scale

--- CV_A1_/A1_image_filtering.py
+++ CV_A1_/A1_image_filtering.py
@@ -208,27 +208,32 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 print(' * filtering for "lenna.png" initiate.')
 img_lenna = cv2.imread('./lenna.png', cv2.IMREAD_GRAYSCALE)
 final_filtered_lenna = np.zeros((img_lenna.shape[0]*3, img_lenna.shape[1]*3))
-'''
+
 for i in range(3):
     for j in range(3):
         k = k_size[i]
         s = sigmas[j]
-        kernel = get_gaussian_filter_2d(k, s)
+        #kernel = get_gaussian_filter_2d(k, s)
+        kernel_h = get_gaussian_filter_1d(k, s)
+        kernel_v = np.array([kernel_h]).transpose()
 
         print('filtering... kernel size: {0}, sigma: {1}'.format(k, s), end='')
-        filtered_img_lenna = cross_correlation_2d(img_lenna, kernel).astype(dtype='uint8')
+        #filtered_img_lenna = cross_correlation_2d(img_lenna, kernel).astype(dtype='uint8')
+        filtered_img_lenna = cross_correlation_1d(img_lenna, kernel_h)
+        filtered_img_lenna = cross_correlation_1d(filtered_img_lenna, kernel_v).astype(dtype='uint8')
         cv2.putText(filtered_img_lenna, '{0}x{0}, s={1}'.format(k, s), (30, 30), font, 1, (0, 0, 0), 2, cv2.LINE_AA)
 
         for x in range(img_lenna.shape[0] * i, img_lenna.shape[0] * (i+1)):
             u = x - img_lenna.shape[0] * i
             for y in range(img_lenna.shape[1] * j, img_lenna.shape[1] * (j+1)):
                 v = y - img_lenna.shape[1] * j
-                final_filtered_lenna[x][y] = filtered_img_lenna[u][v]
+                final_filtered_lenna[x][y] = filtered_img_lenna[u][v] / 255
         print(' ---> done.')
 
 cv2.imwrite('./result/part_1_gaussian_filtered_lenna.png', final_filtered_lenna)
-print(' * filtered image of "lenna.png" saved to ./result/ directory.\n')
+print(' * filtered image of "lenna.png" saved to ./result/ directory.')
 cv2.imshow('Gaussian filtering of "Lenna.png" by Yoseob Kim', final_filtered_lenna)
+print(' ## notice: press any key (on image window) to continue.\n')
 cv2.waitKey(0)
 
 # for "shapes.png"
@@ -240,24 +245,30 @@ for i in range(3):
     for j in range(3):
         k = k_size[i]
         s = sigmas[j]
-        kernel = get_gaussian_filter_2d(k, s)
+        #kernel = get_gaussian_filter_2d(k, s)
+        kernel_h = get_gaussian_filter_1d(k, s)
+        kernel_v = np.array([kernel_h]).transpose()
 
         print('filtering... kernel size: {0}, sigma: {1}'.format(k, s), end='')
-        filtered_img_shapes = cross_correlation_2d(img_shapes, kernel).astype(dtype='uint8')
+        #filtered_img_shapes = cross_correlation_2d(img_shapes, kernel).astype(dtype='uint8')
+        filtered_img_shapes = cross_correlation_1d(img_shapes, kernel_h)
+        filtered_img_shapes = cross_correlation_1d(filtered_img_shapes, kernel_v).astype(dtype='uint8')
         cv2.putText(filtered_img_shapes, '{0}x{0}, s={1}'.format(k, s), (30, 30), font, 1, (0, 0, 0), 2, cv2.LINE_AA)
 
         for x in range(img_shapes.shape[0] * i, img_shapes.shape[0] * (i + 1)):
             u = x - img_shapes.shape[0] * i
             for y in range(img_shapes.shape[1] * j, img_shapes.shape[1] * (j + 1)):
                 v = y - img_shapes.shape[1] * j
-                final_filtered_shapes[x][y] = filtered_img_shapes[u][v]
+                final_filtered_shapes[x][y] = filtered_img_shapes[u][v] / 255
         print(' ---> done.')
 
 cv2.imwrite('./result/part_1_gaussian_filtered_shapes.png', final_filtered_shapes)
-print(' * filtered image of "shapes.png" saved to ./result/ directory.\n')
+print(' * filtered image of "shapes.png" saved to ./result/ directory.')
+
 cv2.imshow('Gaussian filtering of "Shapes.png" by Yoseob Kim', final_filtered_shapes)
+print(' ## notice: press any key (on image window) to continue.\n')
 cv2.waitKey(0)
-'''
+
 
 ## e) comparison btw 1-d kernel correlation superpositioning and 2-d kernel filtering.
 
@@ -284,48 +295,71 @@ print(' ---> done. /elapsed time:', elapsed_time_)
 elapsed_time_ += elapsed_time
 print(' == whole 1-d kernel filtering elapsed time:', elapsed_time_)
 
-cv2.imshow('1-d', filtered_img_1d_lenna.astype(dtype='uint8'))
-cv2.waitKey(0)
+#cv2.imshow('1-d', filtered_img_1d_lenna.astype(dtype='uint8'))
+#cv2.waitKey(0)
 
-print('filtering... (2-d kernel)', end='')
+print('filtering... (2-d kernel), ** time consuming alert (about 62.16sec) **') # , end='')
 start_time = time.process_time()
 filtered_img_2d_lenna = cross_correlation_2d(img_lenna, kernel_2d)
 elapsed_time = time.process_time() - start_time
 print(' ---> done. /elapsed time:', elapsed_time)
-print(' == comparison of elapsed time (1-d time) - (2-d time):', elapsed_time_-elapsed_time, end='\n\n')
+print(' == comparison of elapsed time (1-d time) - (2-d time):', elapsed_time_-elapsed_time)
 
-cv2.imshow('2-d', filtered_img_2d_lenna.astype(dtype='uint8'))
-cv2.waitKey(0)
-
-
+#cv2.imshow('2-d', filtered_img_2d_lenna.astype(dtype='uint8'))
+#cv2.waitKey(0)
 
 subtracted_img = np.zeros((img_lenna.shape[0], img_lenna.shape[1]))
 _sum = 0
 for i in range(img_lenna.shape[0]):
     for j in range(img_lenna.shape[1]):
         subtracted_img[i][j] = filtered_img_1d_lenna[i][j] - filtered_img_2d_lenna[i][j]
-        _sum += subtracted_img[i][j]
+        _sum += abs(subtracted_img[i][j])
 
 cv2.imshow("subtraction of 1-d filtering img and 2-d filtering img", subtracted_img)
-print("summation of subtraction:", round(_sum, 10))
+print(' *** sum of absolute intensity difference:', _sum) # , end='\n\n')
+print(' ## notice: press any key (on image window) to continue.\n')
+cv2.waitKey(0)
 
-
-'''
 # for "shapes.png"
+print(' * for "shapes.png"')
+print('filtering... (1-d horizontal kernel)', end='')
 
-print('filtering with 1-d horizontal kernel for "shapes.png"', end='')
+start_time = time.process_time()
 filtered_img_1d_shapes = cross_correlation_1d(img_shapes, kernel_1d_h)
-print(' ---> done.')
+elapsed_time = time.process_time() - start_time
+print(' ---> done. /elapsed time:', elapsed_time)
 
-print('filtering with 1-d vertical kernel for "shapes.png"', end='')
+print('filtering... (1-d vertical kernel)', end='')
+stqrt_time = time.process_time()
 filtered_img_1d_shapes = cross_correlation_1d(filtered_img_1d_shapes, kernel_1d_v)
-print(' ---> done.')
+elapsed_time_ = time.process_time() - start_time
+print(' ---> done. /elapsed time:', elapsed_time_)
+elapsed_time_ += elapsed_time
+print(' == whole 1-d kernel filtering elapsed time:', elapsed_time_)
 
-print('filtering with 2-d kernel for "shapes.png"', end='')
+#cv2.imshow('1-d', filtered_img_1d_shapes.astype(dtype='uint8'))
+#cv2.waitKey(0)
+
+print('filtering... (2-d kernel), ** time consuming alert (about 50.01sec) **') # , end='')
+start_time = time.process_time()
 filtered_img_2d_shapes = cross_correlation_2d(img_shapes, kernel_2d)
-print(' ---> done.')
-'''
+elapsed_time = time.process_time() - start_time
+print(' ---> done. /elapsed time:', elapsed_time)
+print(' == comparison of elapsed time (1-d time) - (2-d time):', elapsed_time_-elapsed_time)
 
+#cv2.imshow('2-d', filtered_img_2d_shapes.astype(dtype='uint8'))
+#cv2.waitKey(0)
+
+subtracted_img = np.zeros((img_shapes.shape[0], img_shapes.shape[1]))
+_sum = 0
+for i in range(img_shapes.shape[0]):
+    for j in range(img_shapes.shape[1]):
+        subtracted_img[i][j] = filtered_img_1d_shapes[i][j] - filtered_img_2d_shapes[i][j]
+        _sum += abs(subtracted_img[i][j])
+
+cv2.imshow("subtraction of 1-d filtering img and 2-d filtering img", subtracted_img)
+print(' *** sum of absolute intensity difference:', _sum) # , end='\n\n')
+# print(' ## notice: press any key (on image window) to continue.\n')
 
 
 cv2.waitKey(0)
