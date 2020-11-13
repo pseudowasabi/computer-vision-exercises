dbde693343ea85fc9ed58f73d04a158e44cfbaf7
chocolateWasabi <y0s30b.k@gmail.com>
2020-11-13 17:41:16 +0900

    initial settings for cv_a2_

---
+++ CV_A2_/A2_2d_transformations.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+import math
+import time
+import operator
+import os
+
+def get_trasnformed_image(img, M):
+    plane = None
+
+    return plane
+
+
+img_lenna = cv2.imread('./lenna.png', cv2.IMREAD_GRAYSCALE)
---
+++ CV_A2_/A2_homography.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+import math
+import time
+import operator
+import os
+
+def compute_homography(srcP, destP):
+    H = None
+
+    return H
+
