b92c0a182152644b4054977f4095a9b4b6147779
chocolateWasabi <y0s30b.k@gmail.com>
2020-11-15 11:59:29 +0900

    first version of 2-d transformation (upper case input not working)

--- CV_A2_/A2_2d_transformations.py
+++ CV_A2_/A2_2d_transformations.py
@@ -1,3 +1,10 @@
+'''
+Computer vision assignment 2 by Yoseob Kim
+A2_2d_transformation.py
+Implementation of various 2-d transformation.
+GitHub Link: https://github.com/pseudowasabi/computer-vision-exercises/tree/master/CV_A2_
+'''
+
 import cv2
 import numpy as np
 import math
@@ -12,6 +19,28 @@ def get_transformed_image(img, M):
         for j in range(801):
             plane[i][j] = 255
 
+    '''
+    0 0 0 0 0
+    0 0 0 0 0
+    0 0 0 0 0
+    (3, 5) shape image
+    
+    center is (400, 400)
+    left top is (399, 398) correspond to (center.x - shape[0] // 2, center.y - shape[1] // 2)
+    right bottom is (401, 402) correspond to (center.x + shape[0] // 2, center.y + shape[1] // 2)
+    
+    - under condition that assumes all input image should be odd size in both height and width.
+    
+    [solution]
+    1. make homogenous coordinate first for original image.
+    2. calculate transformed coordinate and set intensity in each transformed position.
+    
+    '''
+
+    origin_x_coord = np.zeros((img.shape[0], img.shape[1]))
+    origin_y_coord = np.zeros((img.shape[0], img.shape[1]))
+    # do not use zeros_like (data type would be uint8, however desired data type is at least uint16)
+
     x_range = range(400 - img.shape[0] // 2, 400 + img.shape[0] // 2)
     y_range = range(400 - img.shape[1] // 2, 400 + img.shape[1] // 2)
 
@@ -19,9 +48,21 @@ def get_transformed_image(img, M):
     for x in x_range:
         j = 0
         for y in y_range:
-            plane[x][y] = img[i][j]
+            #plane[x][y] = img[i][j]
+            origin_x_coord[i][j] = x
+            origin_y_coord[i][j] = y
             j += 1
         i += 1
+
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            x_prime = M[0][0] * origin_x_coord[i][j] + M[0][1] * origin_y_coord[i][j] + M[0][2] * 1
+            y_prime = M[1][0] * origin_x_coord[i][j] + M[1][1] * origin_y_coord[i][j] + M[1][2] * 1
+
+            plane[int(x_prime)][int(y_prime)] = img[i][j]
+
+    # plus - reducing artifact (not implemented yet)
+    # check x-dir and y-dir respectively, then fill the blanks along each directions
 
     cv2.arrowedLine(plane, (400, 800), (400, 0), (0, 0, 0), thickness=2, tipLength=0.01)
     cv2.arrowedLine(plane, (0, 400), (800, 400), (0, 0, 0), thickness=2, tipLength=0.01)
@@ -31,60 +72,43 @@ def get_transformed_image(img, M):
 
 img_smile = cv2.imread('./smile.png', cv2.IMREAD_GRAYSCALE)
 
-M = np.identity(3)
-plane = get_transformed_image(img_smile, M)
+current_M = np.identity(3)
+plane = get_transformed_image(img_smile, current_M)
 
 _M = {}     # save all transformation matrix into hash map
-
-_M['translate_left'] = np.array([[1., 0., -5.], [0., 1., 0.], [0., 0., 1.]])
-_M['translate_right'] = np.array([[1., 0., 5.], [0., 1., 0.], [0., 0., 1.]])
-_M['translate_up'] = np.array([[1., 0., 0.], [0., 1., -5.], [0., 0., 1.]])
-_M['translate_down'] = np.array([[1., 0., 0.], [0., 1., 5.], [0., 0., 1.]])
 
 degree_5 = math.pi / 36.
 cos_5 = math.cos(degree_5)
 sin_5 = math.sin(degree_5)
-_M['rotate_ccw'] = np.array([[cos_5, -sin_5, 0.], [sin_5, cos_5, 0.], [0., 0., 1.]])
-_M['rotate_cw'] = np.array([[cos_5, sin_5, 0.], [-sin_5, cos_5, 0.], [0., 0., 1.]])
-_M['flip_y_axis'] = np.array([[-1., 0., 0.], [0., 1., 0.], [0., 0., 1.]])
-_M['flip_x_axis'] = np.array([[1., 0., 0.], [0., -1., 0.], [0., 0., 1.]])
-_M['identity'] = np.identity(3)
-_M['shrink_x_dir'] = np.array([[0.95, 0., 0.], [0., 1., 0.], [0., 0., 1.]])
-_M['enlarge_x_dir'] = np.array([[1.05, 0., 0.], [0., 1., 0.], [0., 0., 1.]])
-_M['shrink_y_dir'] = np.array([[1., 0., 0.], [0., 0.95, 0.], [0., 0., 1.]])
-_M['enlarge_y_dir'] = np.array([[1., 0., 0.], [0., 1.05, 0.], [0., 0., 1.]])
+
+_M[ord('a')] = np.array([[1., 0., 0.], [0., 1., -5.], [0., 0., 1.]])                # move left by 5 pixels
+_M[ord('d')] = np.array([[1., 0., 0.], [0., 1., 5.], [0., 0., 1.]])                 # move right by 5 pixels
+_M[ord('w')] = np.array([[1., 0., -5.], [0., 1., 0.], [0., 0., 1.]])                # move upward by 5 pixels
+_M[ord('s')] = np.array([[1., 0., 5.], [0., 1., 0.], [0., 0., 1.]])                 # move downward by 5 pixels
+_M[ord('r')] = np.array([[cos_5, -sin_5, 0.], [sin_5, cos_5, 0.], [0., 0., 1.]])    # rotate CCW by 5 degrees
+_M[ord('R')] = np.array([[cos_5, sin_5, 0.], [-sin_5, cos_5, 0.], [0., 0., 1.]])    # rotate CW by 5 degrees
+_M[ord('f')] = np.array([[1., 0., 0.], [0., -1., 0.], [0., 0., 1.]])                # flip across y axis
+_M[ord('F')] = np.array([[-1., 0., 0.], [0., 1., 0.], [0., 0., 1.]])                # flip across x axis
+_M[ord('x')] = np.array([[0.95, 0., 0.], [0., 1., 0.], [0., 0., 1.]])               # shrink size by 5% along x dir
+_M[ord('X')] = np.array([[1.05, 0., 0.], [0., 1., 0.], [0., 0., 1.]])               # enlarge size by 5% along x dir
+_M[ord('y')] = np.array([[1., 0., 0.], [0., 0.95, 0.], [0., 0., 1.]])               # shrink size by 5% along y dir
+_M[ord('Y')] = np.array([[1., 0., 0.], [0., 1.05, 0.], [0., 0., 1.]])               # enlarge size by 5% along y dir
 
 # reference for opencv keyboard input (below link)
 # https://subscription.packtpub.com/book/application_development/9781788474443/1/ch01lvl1sec18/handling-user-input-from-a-keyboard
 end = False
 while not end:
+    print(current_M)
+    plane = get_transformed_image(img_smile, current_M)
     cv2.imshow("various 2-d transformations", plane)
     key = cv2.waitKey(0)
-    if key == ord('a'):     # move left by 5 pixels
-        plane = get_transformed_image(img_smile, _M['translate_left'])
-    elif key == ord('d'):   # move right by 5 pixels
-        pass
-    elif key == ord('w'):   # move upward by 5 pixels
-        pass
-    elif key == ord('s'):   # move downward by 5 pixels
-        pass
-    elif key == ord('r'):   # rotate CCW by 5 degrees
-        pass
-    elif key == ord('R'):   # rotate CW by 5 degrees
-        pass
-    elif key == ord('f'):   # flip across y axis
-        pass
-    elif key == ord('F'):   # flip across x axis
-        pass
-    elif key == ord('x'):   # shrink size by 5% along x dir
-        pass
-    elif key == ord('X'):   # enlarge size by 5% along x dir
-        pass
-    elif key == ord('y'):   # shrink size by 5% along y dir
-        pass
-    elif key == ord('Y'):   # enlarge size by 5% along y dir
-        pass
-    elif key == ord('H'):   # set to initial state
-        pass
-    elif key == ord('Q'):   # quit
+    print(key)
+
+    # why upper case input not working ????????????
+    if key == ord('H'):
+        current_M = np.identity(3)
+    elif key == ord('Q'):
         end = True
+    else:
+        if key in _M:
+            current_M = _M[key].dot(current_M)
--- CV_A2_/A2_homography.py
+++ CV_A2_/A2_homography.py
@@ -1,3 +1,10 @@
+'''
+Computer vision assignment 2 by Yoseob Kim
+A2_homography.py
+Implementation of feature matching, homography, RANSAC, image warping.
+GitHub Link: https://github.com/pseudowasabi/computer-vision-exercises/tree/master/CV_A2_
+'''
+
 import cv2
 import numpy as np
 import math
