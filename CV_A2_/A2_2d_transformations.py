7af7d8fd26e57cd9a5bfc42637f08ff6593c418e
pseudowasabi <y0s30b.k@gmail.com>
2020-11-18 00:34:04 +0900

    1st revision of 2-d transformation (basic spec implemented)

--- CV_A2_/A2_2d_transformations.py
+++ CV_A2_/A2_2d_transformations.py
@@ -37,29 +37,47 @@ def get_transformed_image(img, M):
     
     '''
 
-    origin_x_coord = np.zeros((img.shape[0], img.shape[1]))
     origin_y_coord = np.zeros((img.shape[0], img.shape[1]))
+    origin_x_coord = np.zeros((img.shape[0], img.shape[1]))
     # do not use zeros_like (data type would be uint8, however desired data type is at least uint16)
 
-    x_range = range(400 - img.shape[0] // 2, 400 + img.shape[0] // 2)
-    y_range = range(400 - img.shape[1] // 2, 400 + img.shape[1] // 2)
+    y_range = range(400 - img.shape[0] // 2, 400 + img.shape[0] // 2 + 1)
+    x_range = range(400 - img.shape[1] // 2, 400 + img.shape[1] // 2 + 1)
+    #print(y_range, x_range)
+
+    # (400, 400)으로 맞춰두고 좌표 변환을 하지 말고,
+    # 좌표 변환을 다 해놓고 (400, 400)으로 translation 할 것!
+
+    start_y_pos = 400 - img.shape[0] // 2
+    start_x_pos = 400 - img.shape[1] // 2
+
+
+    # (i, 0 -> y), (j, 1 -> x)
 
     i = 0
-    for x in x_range:
+    #for y in range(img.shape[0]):
+    for y in y_range:
         j = 0
-        for y in y_range:
-            #plane[x][y] = img[i][j]
-            origin_x_coord[i][j] = x
+        #for x in range(img.shape[1]):
+        for x in x_range:
             origin_y_coord[i][j] = y
+            origin_x_coord[i][j] = x
             j += 1
         i += 1
 
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
+    #print(origin_y_coord)
+    #print(origin_x_coord)
+
+    for i in range(img.shape[0]):       # y range
+        for j in range(img.shape[1]):   # x range
             x_prime = M[0][0] * origin_x_coord[i][j] + M[0][1] * origin_y_coord[i][j] + M[0][2] * 1
             y_prime = M[1][0] * origin_x_coord[i][j] + M[1][1] * origin_y_coord[i][j] + M[1][2] * 1
 
-            plane[int(x_prime)][int(y_prime)] = img[i][j]
+            #plane[(int(y_prime)+start_y_pos)%801][(int(x_prime)+start_x_pos)%801] = img[i][j]
+            if (int(y_prime) in range(801)) and (int(x_prime) in range(801)):
+                plane[int(y_prime)][int(x_prime)] = img[i][j]
+            else:
+                print("out of range", i, j, origin_y_coord[i][j], origin_x_coord[i][j], y_prime, x_prime)
 
     # plus - reducing artifact (not implemented yet)
     # check x-dir and y-dir respectively, then fill the blanks along each directions
@@ -81,18 +99,32 @@ degree_5 = math.pi / 36.
 cos_5 = math.cos(degree_5)
 sin_5 = math.sin(degree_5)
 
-_M[ord('a')] = np.array([[1., 0., 0.], [0., 1., -5.], [0., 0., 1.]])                # move left by 5 pixels
-_M[ord('d')] = np.array([[1., 0., 0.], [0., 1., 5.], [0., 0., 1.]])                 # move right by 5 pixels
-_M[ord('w')] = np.array([[1., 0., -5.], [0., 1., 0.], [0., 0., 1.]])                # move upward by 5 pixels
-_M[ord('s')] = np.array([[1., 0., 5.], [0., 1., 0.], [0., 0., 1.]])                 # move downward by 5 pixels
-_M[ord('r')] = np.array([[cos_5, -sin_5, 0.], [sin_5, cos_5, 0.], [0., 0., 1.]])    # rotate CCW by 5 degrees
-_M[ord('R')] = np.array([[cos_5, sin_5, 0.], [-sin_5, cos_5, 0.], [0., 0., 1.]])    # rotate CW by 5 degrees
-_M[ord('f')] = np.array([[1., 0., 0.], [0., -1., 0.], [0., 0., 1.]])                # flip across y axis
-_M[ord('F')] = np.array([[-1., 0., 0.], [0., 1., 0.], [0., 0., 1.]])                # flip across x axis
+_M[ord('a')] = np.array([[1., 0., -5.], [0., 1., 0.], [0., 0., 1.]])                # move left by 5 pixels
+_M[ord('d')] = np.array([[1., 0., +5.], [0., 1., 0.], [0., 0., 1.]])                 # move right by 5 pixels
+_M[ord('w')] = np.array([[1., 0., 0.], [0., 1., -5.], [0., 0., 1.]])                # move upward by 5 pixels
+_M[ord('s')] = np.array([[1., 0., 0.], [0., 1., +5.], [0., 0., 1.]])                 # move downward by 5 pixels
+_M[ord('R')] = np.array([[cos_5, -sin_5, 0.], [sin_5, cos_5, 0.], [0., 0., 1.]])    # rotate CW by 5 degrees
+_M[ord('r')] = np.array([[cos_5, sin_5, 0.], [-sin_5, cos_5, 0.], [0., 0., 1.]])    # rotate CCW by 5 degrees
+_M[ord('F')] = np.array([[1., 0., 0.], [0., -1., 0.], [0., 0., 1.]])                # flip across x axis
+_M[ord('f')] = np.array([[-1., 0., 0.], [0., 1., 0.], [0., 0., 1.]])                # flip across y axis
 _M[ord('x')] = np.array([[0.95, 0., 0.], [0., 1., 0.], [0., 0., 1.]])               # shrink size by 5% along x dir
 _M[ord('X')] = np.array([[1.05, 0., 0.], [0., 1., 0.], [0., 0., 1.]])               # enlarge size by 5% along x dir
 _M[ord('y')] = np.array([[1., 0., 0.], [0., 0.95, 0.], [0., 0., 1.]])               # shrink size by 5% along y dir
 _M[ord('Y')] = np.array([[1., 0., 0.], [0., 1.05, 0.], [0., 0., 1.]])               # enlarge size by 5% along y dir
+
+translate_minus_400 = np.array([[1., 0., -400.], [0., 1., -400.], [0., 0., 1.]])
+translate_plus_400 = np.array([[1., 0., +400.], [0., 1., +400.], [0., 0., 1.]])
+
+#_M[ord('R')] = np.matmul(translate_plus_400, np.matmul(_M[ord('R')], translate_minus_400))
+#_M[ord('r')] = np.matmul(translate_plus_400, np.matmul(_M[ord('r')], translate_minus_400))
+#_M[ord('y')] = np.matmul(test2, np.matmul(_M[ord('y')], test1))
+#_M[ord('Y')] = np.matmul(test2, np.matmul(_M[ord('Y')], test1))
+#_M[ord('x')] = np.matmul(test2, np.matmul(_M[ord('x')], test1))
+#_M[ord('X')] = np.matmul(test2, np.matmul(_M[ord('X')], test1))
+
+for mat in _M:
+    _M[mat] = np.matmul(translate_plus_400, np.matmul(_M[mat], translate_minus_400))
+
 
 # reference for opencv keyboard input (below link)
 # https://subscription.packtpub.com/book/application_development/9781788474443/1/ch01lvl1sec18/handling-user-input-from-a-keyboard
@@ -111,4 +143,6 @@ while not end:
         end = True
     else:
         if key in _M:
-            current_M = _M[key].dot(current_M)
+            current_M = np.matmul(_M[key], current_M)
+
+cv2.destroyAllWindows()
