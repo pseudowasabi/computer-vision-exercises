34b7c8402c32dbb00e484f90780ebb6546a3f8dc
pseudowasabi <y0s30b.k@gmail.com>
2020-12-15 16:57:17 +0900

    read binary data of SIFT descriptors

--- CV_A4_/A4_compute_descriptors.py
+++ CV_A4_/A4_compute_descriptors.py
@@ -15,16 +15,42 @@ import operator
 import random
 
 img = cv2.imread('ukbench00000.jpg', cv2.IMREAD_GRAYSCALE)
-f = open('./sift/sift100000', 'rb')
-line1 = f.readline()
-line2 = f.readline()
-line3 = f.readline()
 
-print(line1)
-print(len(line1))
-print(line2)
-print(len(line2))
-print(line3)
-print(len(line3))
+'''
+my_min = np.inf
+my_max = 0'''
+for i in range(1000):
+    offset = '00' if i < 10 else '0' if i < 100 else ''
+    offset += str(i)
+    #print(offset)
+
+    f = open('./sift/sift100'+offset, 'rb')
+
+    # reference - https://numpy.org/doc/stable/reference/generated/numpy.frombuffer.html
+    sift_des = np.frombuffer(f.read(), dtype=np.uint8)
+    #print(sift_des.shape)
+    #print(sift_des)
+
+    '''
+    if sift_des.shape[0] % 128 != 0:
+        print('divide error')
+    '''
+    sift_des_reshaped = np.reshape(sift_des, (sift_des.shape[0] // 128, 128))
+    #print(sift_des_reshaped.shape)
+
+    '''
+    if sift_des_reshaped.shape[0] < my_min:
+        my_min = sift_des_reshaped.shape[0]
+    if sift_des_reshaped.shape[0] > my_max:
+        my_max = sift_des_reshaped.shape[0]'''
+
+    f.close()
+
+
+#print(my_min, my_max)
+# N size
+# min = 73, max = 2388
+
+
 
 
